9a84d067a338ae08bcfe00b3b9752bbe6c7ee181
getuliot <69749513+getuliot@users.noreply.github.com>
2021-02-07 12:19:10 -0300

    Monitoração de Temperatura

---
+++ Monitoração de Temperatura DHT22.ino
@@ -0,0 +1,102 @@
+/*
+     Q0482-Sketch.ino
+     AUTOR:   BrincandoComIdeias
+     LINK:    https://www.youtube.com/brincandocomideias ; https://cursodearduino.net/
+     COMPRE:  https://www.arducore.com.br/
+     SKETCH:  Data Logger com Envio ao Excel via Teclado (Para Arduino Leonardo ou Compatível)
+     DATA:    20/07/2019
+
+   ATUALIZACAO: em 07/02/2021 por Getulio T. Tateoki o que retira o relogio de tempo real
+*/
+//SENSOR DE TEMPERATURA E HUMIDADE RELATIVA
+#include <dht.h>
+
+//OBJETO PARA EMULADOR DE TECLADO (ARDUINO LEONARDO OU COMPATIVEL)
+#include <Keyboard.h>
+
+//CRIACAO DOS OBJETOS
+dht DHT;
+
+//PINAGEM DO CIRCUITO
+#define pinPause 10
+#define DHT22_PIN 4
+
+//DECLARACAO DAS FUNCOES
+void estadoPause();
+void erroCritico();
+void teclaEnter();
+void teclaTAB();
+
+//EXECUTADO QUANDO O ARDUINO FOR LIGADO
+void setup() {
+  //INICIO DOS PINOS
+  pinMode(pinPause, INPUT_PULLUP);
+
+  //INICIO DO EMULADOR DE TECLADO
+  Keyboard.begin();
+}
+
+//EXECUTADO REPETIDAMENTE APÓS A EXECUCAO DO SETUP
+void loop() {
+
+  //PARA FACILITAR FUTURAS CARGAS
+  if (!digitalRead(pinPause)) {
+    estadoPause();
+    return;
+  }
+
+  int chk = DHT.read22(DHT22_PIN);
+  if (chk == DHTLIB_OK) {
+
+    String umidade_string;
+    umidade_string = String(DHT.humidity, 2);
+    umidade_string.replace(".", ",");
+
+    Keyboard.print(umidade_string);
+    teclaTAB();
+
+    String temperatura_string;
+    temperatura_string = String(DHT.temperature, 2);
+    temperatura_string.replace(".", ",");
+
+    Keyboard.print(temperatura_string);
+    teclaTAB();
+  }
+
+  teclaEnter();
+  delay(2000);
+}
+
+//FUNCAO PARA AGUARDAR ANTES DE INICIAR O SETUP PARA NAO PROJUDICAR FUTURAS CARGAS
+void estadoPause() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(900);                       // wait for a second
+}
+
+//FUNCAO PARA AVISO DE ERRO CRITICO ATRAVES DO LED INTERNO DO ARDUINO
+void erroCritico() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (true) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(200);                       // wait for a second
+  }
+}
+
+//FUNCAO PARA ENVIAR O APERTO DE UM ENTER
+void teclaEnter() {
+  Keyboard.press(KEY_RETURN);   // PRESSIONA O ENTER
+  delay(50);                   // ESPERA 100 MILISSEGUNDOS
+  Keyboard.release(KEY_RETURN); // SOLTA O ENTER
+}
+
+//FUNCAO PARA ENVIAR O APERTO DE UM TAB
+void teclaTAB() {
+  Keyboard.press(KEY_TAB);   // PRESSIONA O TAB
+  delay(50);                // ESPERA 100 MILISSEGUNDOS
+  Keyboard.release(KEY_TAB); // SOLTA O TAB
+}
